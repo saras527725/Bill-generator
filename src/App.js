90dcd1b77ba591c449094e9ba07ee01f4775a12c
SARAS KUMAR <saraskumar52772525@gmail.com>
2024-11-27 02:11:19 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React ,{useState}from 'react';
+import BillDetails from './components/BillDetails';
+import ItemList from './components/ItemList';
+import TotalAmount from './components/TotalAmount';
+import { jsPDF } from 'jspdf';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() 
+{
+  const [items, setItems] = useState([]);
+
+
+    const handleAddItem = (item) => {
+        setItems([...items, item]);
+    };
+
+    const handleDeleteItem = (index) => {
+        const updatedItems = [...items];
+        updatedItems.splice(index, 1);
+        setItems(updatedItems);
+    };
+
+    const calculateTotalAmount = () => {
+        return items.reduce(
+            (total, item) =>
+                total +
+                item.quantity *
+                item.price, 0);
+    };
+
+    const handleDownloadPDF = () => {
+        const pdf = new jsPDF();
+        pdf.text('Invoice', 20, 20);
+
+        // Add items to PDF
+        items.forEach((item, index) => {
+            const yPos = 30 + index * 10;
+            pdf.text(
+                `Item: ${item.item}, 
+                    Quantity: ${item.quantity}, 
+                    Price: ${item.price}`, 20, yPos);
+        });
+
+        // Add total amount to PDF
+        const totalAmount =
+            calculateTotalAmount();
+        pdf.text(
+            `Total Amount: 
+                    $${totalAmount.toFixed(2)}`, 20, 180);
+
+        // Save the PDF
+        pdf.save('invoice.pdf');
+    };
+
+    return <>
+        <div className="App">
+            <h1>Bill/Invoice Generator</h1>
+            <BillDetails onAddItem={handleAddItem} />
+            <ItemList items={items}
+                onDeleteItem={handleDeleteItem} />
+            <TotalAmount
+                total={calculateTotalAmount()} />
+            <button
+                onClick={handleDownloadPDF}>Download PDF</button>
+        </div>
+    </>
 }
 
 export default App;
